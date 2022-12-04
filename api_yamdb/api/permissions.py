4330b3a2258414063b6a15012156428fcd389a5e
Арсен Григорян <arsen8577711@yandex.ru>
2022-12-04 18:24:15 +0300

    Сделали титлы, комменты, ревьюхи

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -14,7 +14,16 @@ class AdminOnly(permissions.BasePermission):
 class AdminSuperUserOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        role_admin_moder = ('SUPRUSER', 'ADMIN')
-        if request.user.role in role_admin_moder:
+        if request.user.is_authenticated and request.user.is_staff \
+                or request.user.role == 'ADMIN':
             return True
         return False
+
+
+# class GeneralPermission(permissions.BasePermission):
+#
+#     def has_permission(self, request, view):
+#         return bool(request.user.is_authenticated and
+#                     (request.user.is_staff or
+#                      request.user.role == UserRole.ADMIN) or
+#                     request.method in permissions.SAFE_METHODS)
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,7 +9,7 @@ from .views import (
     SignUpViewSet,
     GenresViewSet,
     CategoriesViewSet,
-    TokenViewSet
+    get_token
     )
 
 
@@ -17,26 +17,22 @@ router_v1 = routers.DefaultRouter()
 router_v1.register(r'titles', TitleViewSet, basename='titles')
 router_v1.register(r'genres', GenresViewSet, basename='genres')
 router_v1.register(r'categories', CategoriesViewSet, basename='categories')
-router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet,
-    basename='reviews'
-)
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   ReviewViewSet, basename='review')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/',
     CommentsViewSet,
     basename='comments'
 )
-# router_v1.register(r'auth/token', TokenViewSet)
-# router_v1.register(r'auth', TitleViewSet, basename='auth')
+
 router_v1.register(r'users/me/', MeViewSet, basename='me')
 router_v1.register(r'users', UsersViewSet, basename='users')
 router_v1.register(r'auth/signup', SignUpViewSet, basename='signup')
-router_v1.register(r'auth/token', TokenViewSet, basename='token')
+# router_v1.register(r'auth/token', get_token, basename='token')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    # path('v1/auth/signup/', signup, name='signup'),
-    # path('v1/auth/token/', token, name='login'),
-    # path('v1/auth/code/', code, name='code'),
+    # path('v1/auth/email/', send_confirmation_code),
+    path('v1/auth/token/', get_token),
+    # path('v1/users/me/', UserInfo.as_view()),
 ]
